8d450cbb9e52f72ded2a5dd78137c06eff9dc9d4
Johan Emanuel <johanmendex2@gmail.com>
2020-11-03 14:27:35 -0500

    first commit

---
+++ src/main/java/com/pukasoft/springDesdeCero/SpringDesdeCeroApplication.java
@@ -0,0 +1,13 @@
+package com.pukasoft.springDesdeCero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDesdeCeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDesdeCeroApplication.class, args);
+	}
+
+}
